11b46d8ac05501f61ab534a9268a3cc0bbb96667
Abban <himself@abandon.ie>
2012-09-04 00:08:58 +0100

    Committing current WIP

--- jquery-responsetabs.js
+++ jquery-responsetabs.js
@@ -1,6 +1,6 @@
 /**
- * jQuery Breakpoints
- * http://github.com/Abban/jQuery-Breakpoints
+ * jQuery ResponseTabs
+ * http://github.com/Abban/jQuery-ResponseTabs
  * 
  * August 2012
  * 
@@ -8,21 +8,70 @@
  * @author Abban Dunne http://abandon.ie
  * @license MIT
  *
- * Fires off events based on given breakpoints when the browser window is resized.
- * Makes it easier to add customisable jQuery to your site.
+ * Adds a 100% width content slider to allow iOS style horizontal sliding transitions.
  * 
  */
 (function($){
 
-	$.fn.responsetabs = function(args){
+	$.fn.responseTabs = function(args){
 
 		var defaults = {
 
-			container : false
+			navigation : false,
+			speed      : 500,
+			easing     : 'linear'
 
-        };
-		
+        };		
 		var settings = $.extend({}, defaults, args);
+
+		var $this = this;
+
+		var slider = $('>div', this);
+
+		var tabCount = $('>div >div', this).size();
+
+		// Apply CSS to container and child elements
+		this.css({
+			width    : '100%',
+			overflow : 'hidden',
+			position : 'relative',
+			height   : $('>div >div:first-child', this).height()
+		});
+
+		$(slider).css({
+			width    : (100*tabCount) + '%',
+			position : 'absolute'
+		});
+
+		$('>div >div', this).css({
+			width : (100/tabCount) + '%',
+			clear : 'none',
+			float : 'left' 
+		});
+
+		if(settings.navigation)
+			$(settings.navigation).show().find('li:first-child a').addClass('current');
+
+		// Add click event to navigation
+		$(settings.navigation + ' a').click(function(){
+			
+			$(this).addClass('current').parent().siblings().find('a').removeClass('current');
+
+			var index = $(this).parent().index();
+
+			$this.animate({
+				height : $('>div:nth-child('+ (index+1) +')', slider).height()
+			}, settings.speed);
+
+			slider.stop().animate({
+
+				'left' : index*-100 + '%'
+
+			}, settings.speed, settings.easing);
+
+			return false;
+		});
+
 
 	};
 

4a0b86911b53b06c5cc69243d02b7b5550fa8bd5
Abban <himself@abandon.ie>
2012-09-03 22:03:49 +0100

    Adding base files

---
+++ jquery-responsetabs.js
@@ -0,0 +1,29 @@
+/**
+ * jQuery Breakpoints
+ * http://github.com/Abban/jQuery-Breakpoints
+ * 
+ * August 2012
+ * 
+ * @version 0.1
+ * @author Abban Dunne http://abandon.ie
+ * @license MIT
+ *
+ * Fires off events based on given breakpoints when the browser window is resized.
+ * Makes it easier to add customisable jQuery to your site.
+ * 
+ */
+(function($){
+
+	$.fn.responsetabs = function(args){
+
+		var defaults = {
+
+			container : false
+
+        };
+		
+		var settings = $.extend({}, defaults, args);
+
+	};
+
+})(jQuery);
